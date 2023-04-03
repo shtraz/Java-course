0876db59b07c300a02a4feb808c22a6994c7c291
shtraz <mr.ushak0v.m@gmail.com>
2023-04-03 22:12:04 +0300

    less_1

---
+++ Lectures/lesson_1/sravnenie.java
@@ -0,0 +1,14 @@
+public class sravnenie {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+//        System.out.println(a | b);
+//        System.out.println(a & b);
+        System.out.println(a ^ b);
+        // 101 = 5
+        // 010 = 2 (0 середи добавили )
+        // 111 = 7 итог
+        // 000 = 0 если &
+
+    }
+}
