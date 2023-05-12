f0dd6a3f266f4a0d294b79745fdda51c24f01247
shtraz <mr.ushak0v.m@gmail.com>
2023-05-13 00:10:35 +0300

    add HW_less_3

---
+++ Practice/lesson_3/less_3_2.java
@@ -0,0 +1,27 @@
+package lesson_3;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+//Пусть дан произвольный список целых чисел, удалить из него чётные числа
+public class less_3_2 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(0, 100);
+            list1.add(val);
+        }
+        System.out.printf("Исходный список %s\n", list1);
+        int i = 0;
+
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.printf("Итоговый список %s", list1);
+    }
+}
